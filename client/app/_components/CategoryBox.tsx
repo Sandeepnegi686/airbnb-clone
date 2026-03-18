4d08def7544c2d832fd2707edb3a2705e86f725d
Sandeep Negi <39149906+Sandeepnegi686@users.noreply.github.com>
2026-03-18 18:45:51 +0530

    completed the listing.

---
+++ client/app/_components/CategoryBox.tsx
@@ -0,0 +1,47 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback } from "react";
+import { IconType } from "react-icons";
+import qs from "querystring";
+
+interface CategoryBoxProps {
+  label: string;
+  description: string;
+  icon: IconType;
+  selected?: boolean;
+}
+
+export default function CategoryBox({
+  label,
+  description,
+  icon: Icon,
+  selected,
+}: CategoryBoxProps) {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+    const queryString = qs.stringify(updatedQuery);
+    router.push(`/?${queryString}`);
+  }, [label, params, router]);
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${selected ? "border-b-neutral-800" : "border-transparent"} ${selected ? "text-neutral-800" : "text-neutral-500"}`}
+      onClick={handleClick}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">{label}</div>
+    </div>
+  );
+}
