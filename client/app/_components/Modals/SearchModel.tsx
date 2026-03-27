4bbb0ee2c4babb68d786b5fe56cc368ec6b5765e
Sandeep Negi <39149906+Sandeepnegi686@users.noreply.github.com>
2026-03-27 23:09:00 +0530

    updated code

--- client/app/_components/Modals/SearchModel.tsx
+++ client/app/_components/Modals/SearchModel.tsx
@@ -7,9 +7,11 @@ import Model from "./Model";
 import { Range } from "react-date-range";
 import dynamic from "next/dynamic";
 import qs from "querystring";
-import { CountrySelectValue } from "../Inputs/CountrySelect";
+import CountrySelect, { CountrySelectValue } from "../Inputs/CountrySelect";
 import { formatISO } from "date-fns";
-import { url } from "inspector";
+import Heading from "../Heading";
+import Calender from "../Inputs/Calender";
+import Counter from "../Inputs/Counter";
 
 enum STEP {
   LOCATION = 0,
@@ -65,7 +67,7 @@ export default function SearchModel() {
       updatedQuery.startDate = formatISO(dateRange.startDate);
     }
     if (dateRange.endDate) {
-      updatedQuery.startDate = formatISO(dateRange.endDate);
+      updatedQuery.endDate = formatISO(dateRange.endDate);
     }
     const queryString = qs.stringify(updatedQuery);
     setStep(STEP.LOCATION);
@@ -90,16 +92,80 @@ export default function SearchModel() {
       return "Search";
     }
     return "Next";
-  });
-  const secondaryAci;
+  }, [step]);
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEP.LOCATION) {
+      return undefined;
+    }
+    return "BACK";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Where do you wanna go?"
+        subtitle="Find the perfect location!"
+      />
+      <CountrySelect
+        value={location}
+        onChange={(value) => setLocation(value as CountrySelectValue)}
+      />
+      <div className="h-px rounded-full bg-neutral-200"></div>
+      <Map center={location?.latlng} />
+    </div>
+  );
+
+  if (step === STEP.DATE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="When do you plan to go?"
+          subtitle="Make sure everyone is free!"
+        />
+        <Calender
+          value={dateRange}
+          onChange={(value) => setDateRange(value.selection)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEP.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="More information" subtitle="Find your perfect place" />
+        <Counter
+          title="Guests"
+          subtitle="How many guests are coming?"
+          onChange={(value) => setGuestCount(value)}
+          value={guestCount}
+        />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you need?"
+          onChange={(value) => setRoomCount(value)}
+          value={roomCount}
+        />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you need?"
+          onChange={(value) => setBathroomCount(value)}
+          value={bathroomCount}
+        />
+      </div>
+    );
+  }
 
   return (
     <Model
       isOpen={searchModel.isOpen}
       onClose={searchModel.setClose}
-      onSubmit={searchModel.setOpen}
+      onSubmit={onSubmit}
       title="Filters"
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryAction={step === STEP.LOCATION ? undefined : onBack}
+      secondaryActionLabel={secondaryActionLabel}
+      body={bodyContent}
     />
   );
 }
--- client/app/api/listing/[listingId]/route.ts
+++ client/app/api/listing/[listingId]/route.ts
@@ -1,31 +1,34 @@
+import { NextRequest } from "next/server";
 import BASE_API_URL from "@/app/lib/api";
 import { cookies } from "next/headers";
 
-interface IParams {
-  listingId?: string;
-}
-
 export async function DELETE(
-  request: Request,
-  { params }: { params: IParams },
+  request: NextRequest,
+  context: { params: { listingId: string } },
 ) {
   const cookieStore = cookies();
   const token = (await cookieStore).get("access-token");
+
   if (!token) {
-    return Response.json({ sucess: false }, { status: 401 });
+    return Response.json({ success: false }, { status: 401 });
   }
-  const listingId = (await params)?.listingId;
+
+  const listingId = context.params.listingId;
+
   if (!listingId) {
-    return Response.json({ sucess: false }, { status: 401 });
+    return Response.json({ success: false }, { status: 400 });
   }
+
   const response = await fetch(`${BASE_API_URL}/api/v1/listing/${listingId}`, {
     method: "DELETE",
     headers: {
-      Cookie: `access-token=${token?.value}`,
+      Cookie: `access-token=${token.value}`,
       "Content-Type": "application/json",
     },
     cache: "no-store",
   });
+
   const data = await response.json();
-  return Response.json(data, { status: 401 });
+
+  return Response.json(data, { status: response.status });
 }
